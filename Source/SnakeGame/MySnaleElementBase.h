322881dbe1178f672fd0ae79c00129b6b4bda827
Kirill Vorozhishchev <1507kirill1998@mail.ru>
2021-09-13 21:04:09 +0700

    6

---
+++ Source/SnakeGame/MySnaleElementBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SnaleElementBae.h"
+#include "MySnaleElementBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNAKEGAME_API AMySnaleElementBase : public ASnaleElementBae
+{
+	GENERATED_BODY()
+	
+};
