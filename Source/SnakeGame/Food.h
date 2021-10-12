ae0793b4933e129fde1a9adc5eb5ac6868fcf177
Kirill Vorozhishchev <1507kirill1998@mail.ru>
2021-10-13 00:41:03 +0700

    15

--- Source/SnakeGame/Food.h
+++ Source/SnakeGame/Food.h
@@ -20,6 +20,9 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+private:
+	
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
